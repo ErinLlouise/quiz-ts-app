9359cc92b896d214f11f0badceb2916deef2f9d9
Erin <erinllouise3@gmail.com>
2021-10-07 09:33:42 +1300

    comp setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+import Home from './components/Home'
+import Quiz from './components/Quiz'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          this is the goods
-        </p>
-      </header>
+      <Route exact path='/' component={Home} />
+      <Route path='/quiz' component={Quiz} />
     </div>
   )
 }
