cd745651baf88c19067f00804933379675018894
Erin <erinllouise3@gmail.com>
2021-10-07 09:27:37 +1300

    add react types

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+          <h1>Ye</h1>
+          <img src='poppyimg.png' alt='poppy' />
+        </>
+    )
+}
+
+export default Home
